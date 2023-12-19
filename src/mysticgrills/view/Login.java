77a8316666798e585bfc0409901c692a142c43ae
Fabian Habil Ramdhan <fabianhabilramdhan@gmail.com>
2023-12-19 09:24:03 +0700

    feat: now login redirecting based on role (for now only admin and user)

---
+++ src/mysticgrills/view/Login.java
@@ -0,0 +1,118 @@
+package mysticgrills.view;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import mysticgrills.GlobalState;
+import mysticgrills.controller.UserController;
+import mysticgrills.model.User;
+import mysticgrills.utils.Dialog;
+import mysticgrills.view.home.AdminHome;
+import mysticgrills.view.home.CustomerHome;
+import mysticgrills.view.user.ManageUser;
+
+public class Login extends BorderPane {
+
+	private UserController userController = UserController.getUC();
+	private GlobalState gs = GlobalState.getInstance();
+
+	VBox container;
+	VBox emailBox;
+	VBox passwordBox;
+	Label title, email, pass, changePage;
+	TextField emailField;
+	PasswordField passField;
+	Button loginButton;
+	Dialog dg;
+
+	// Initialize all component
+	public void initialize() {
+		container = new VBox(8);
+		emailBox = new VBox(4);
+		passwordBox = new VBox(4);
+
+		emailField = new TextField();
+		passField = new PasswordField();
+
+		title = new Label("MysticGrills Login");
+		email = new Label("Email");
+		pass = new Label("Password");
+
+		loginButton = new Button("Login");
+		changePage = new Label("Dont have any account? Register");
+		dg = new Dialog();
+
+	}
+
+	public void style() {
+		title.setFont(new Font(24));
+		loginButton.setMinWidth(180);
+
+		// Email Container
+		emailBox.setMaxWidth(300);
+		emailBox.getChildren().addAll(email, emailField);
+
+		// Password Container
+		passwordBox.setMaxWidth(300);
+		passwordBox.getChildren().addAll(pass, passField);
+
+		// Page Container
+		container.getChildren().addAll(title, emailBox, passwordBox, loginButton, changePage);
+		container.setAlignment(Pos.CENTER);
+	}
+
+	public void listenerFunction(Stage stage) {
+		loginButton.setOnMouseClicked(e -> {
+			String status = userController.authenticateUser(emailField.getText(), passField.getText());
+
+			if (status.contains("Success")) {
+				if (dg.informationDialog("Success", "Success", status)) {
+					redirectUser(stage);
+				}
+			} else {
+				dg.informationDialog("Fail", "Fail", status);
+			}
+		});
+
+		changePage.setOnMouseClicked(e -> {
+			stage.setScene(new Scene(new Register(stage), 1366, 768));
+		});
+	}
+
+	public void redirectUser(Stage stage) {
+		User user = gs.getCurrentLoggedInUser();
+		BorderPane bp;
+
+		if (user.getUserRole().equals("Customer")) {
+			bp = new CustomerHome(stage);
+		}
+
+		else if (user.getUserRole().equals("Admin")) {
+			bp = new AdminHome(stage);
+		}
+
+		else {
+			bp = new BorderPane();
+		}
+
+		stage.setScene(new Scene(bp, 1366, 768));
+	}
+
+	public Login(Stage stage) {
+		stage.setTitle("Login");
+		initialize();
+		style();
+		setCenter(container);
+		listenerFunction(stage);
+
+	}
+
+}
--- src/mysticgrills/view/Register.java
+++ src/mysticgrills/view/Register.java
@@ -1,5 +1,4 @@
-package mysticgrills.view.home;
-
+package mysticgrills.view;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -10,41 +9,48 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import mysticgrills.GlobalState;
 import mysticgrills.controller.UserController;
 import mysticgrills.utils.Dialog;
 
 public class Register extends BorderPane {
 
 	private UserController userController = UserController.getUC();
-	private GlobalState gb = GlobalState.getInstance();
 
-	public Register(Stage stage) {
-		VBox container = new VBox();
-		Label title = new Label("Register");
-		TextField usernameField = new TextField();
-		TextField emailField = new TextField();
-		PasswordField passField = new PasswordField();
-		PasswordField passConfirmField = new PasswordField();
+	VBox container;
+	Label title, username, email, pass, passConfirm, changePage;
+	TextField usernameField, emailField;
+	PasswordField passField, passConfirmField;
+	Button registerButton;
+	Dialog dg;
 
-		Label username = new Label("Username");
-		Label email = new Label("Email");
-		Label pass = new Label("Password");
-		Label passConfirm = new Label("Password Confirmation");
-		Button registerButton = new Button("Register");
-		Label changePage = new Label("Already have an account? Login");
-		Dialog dg = new Dialog();
+	public void initialize() {
+		container = new VBox();
+		title = new Label("Register");
+		usernameField = new TextField();
+		emailField = new TextField();
+		passField = new PasswordField();
+		passConfirmField = new PasswordField();
+
+		username = new Label("Username");
+		email = new Label("Email");
+		pass = new Label("Password");
+		passConfirm = new Label("Password Confirmation");
+		registerButton = new Button("Register");
+		changePage = new Label("Already have an account? Login");
+		dg = new Dialog();
 
 		container.getChildren().addAll(title, username, usernameField, email, emailField, pass, passField, passConfirm,
 				passConfirmField, registerButton, changePage);
 		container.setPadding(new Insets(10));
+	}
 
+	public void listenerFunction(Stage stage) {
 		registerButton.setOnMouseClicked(e -> {
 			String status = userController.createUser("user", usernameField.getText(), emailField.getText(),
 					passField.getText(), passConfirmField.getText());
 
 			if (status.contains("successfully")) {
-				if(dg.informationDialog("Message", "Success", status)) {
+				if (dg.informationDialog("Message", "Success", status)) {
 					System.out.println("The user clicked OK");
 					stage.setScene(new Scene(new Login(stage), 1366, 768));
 				}
@@ -56,7 +62,12 @@ public class Register extends BorderPane {
 		changePage.setOnMouseClicked(e -> {
 			stage.setScene(new Scene(new Login(stage), 1366, 768));
 		});
+	}
 
+	public Register(Stage stage) {
+		stage.setTitle("Register");
+		initialize();
+		listenerFunction(stage);
 		setCenter(container);
 	}
 
